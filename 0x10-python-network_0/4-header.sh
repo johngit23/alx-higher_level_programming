e950458c7714d3c244252f2e19d5a3e45077a632
johngit23 <jhon.klaus2008@gmail.com>
2023-10-01 00:02:00 -0700

    edit 4 header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1" | cat
+URL=$1
+HEADER="X-School-User-Id: 98"
+curl -H "$HEADER" "$URL" | cat
